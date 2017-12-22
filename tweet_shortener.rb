008bca0b5960cad23d8a8855a6cd6daa84811953
osama miro <o.miro650@gmail.com>
2017-12-22 20:47:12 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def word_substituter(string)
+  dictionary = {:hello => "hi",
+  :to => "2",
+:two => "2",
+:too => "2",
+:for => "4",
+:four => "4",
+:be => "b",
+:you => "u",
+:at => "@",
+:and => "&"
+}
+string.collect do |i|
+  dictionary.collect do |key, value|
+    if i == key
+      i = value
+    end
+  end
+end
+string
+end
