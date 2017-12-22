95e7dac3cf340f3d6b247fcc0fbf16b3fef34d12
osama miro <o.miro650@gmail.com>
2017-12-22 21:23:57 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,22 +1,51 @@
 # Write your code here.
-def word_substituter(string)
-  dictionary = {:hello => "hi",
-  :to => "2",
-:two => "2",
-:too => "2",
-:for => "4",
-:four => "4",
-:be => "b",
-:you => "u",
-:at => "@",
-:and => "&"
+def dictionary
+  dictionary = {
+"hello" => "hi",
+"to" => "2",
+"two" => "2",
+"too" => "2",
+"for" => "4",
+"four" => "4",
+"be" => "b",
+"you" => "u",
+"at" => "@",
+"and" => "&"
 }
-string.collect do |i|
-  dictionary.collect do |key, value|
-    if i == key
-      i = value
+end
+
+def word_substituter(string)
+  string = string.dup
+  dictionary.each do |key, value|
+    if string.include?(key)
+      string.gsub!(/\b#{key}\b/i, value)
     end
-  end
+
 end
 string
+end
+
+
+  def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+   word_substituter(tweet)
+ else
+   tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + '...'
+  else
+    tweet
+  end
 end
