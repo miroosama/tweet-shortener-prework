5cbcde739d1b30669b80fb1390c022fbded526f7
osama miro <o.miro650@gmail.com>
2018-01-02 22:03:25 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,37 +15,20 @@ def dictionary
 end
 
 def word_substituter(string)
-  string = string.dup
-  dictionary.each do |key, value|
-    if string.include?(key)
-      string.gsub!(/\b#{key}\b/i, value)
-    end
 
-end
-string
 end
 
 
   def bulk_tweet_shortener(tweets)
-  tweets.each do |tweet|
-    puts word_substituter(tweet)
-  end
+
 end
 
 
 
 def selective_tweet_shortener(tweet)
-  if tweet.length > 140
-   word_substituter(tweet)
- else
-   tweet
-  end
+
 end
 
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0..136] + '...'
-  else
-    tweet
-  end
+
 end
